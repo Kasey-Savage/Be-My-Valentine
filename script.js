9710a93cd77edafee672331a09aee65289d6b797
Kasey-Savage <103228559+Kasey-Savage@users.noreply.github.com>
2025-01-27 16:32:48 -0800

    added script file for button transition

---
+++ script.js
@@ -0,0 +1,13 @@
+function handleYes() {
+    document.body.style.backgroundColor = "#ff3838";
+    document.getElementById("response").innerHTML = "Yay! I knew you'd say yes! 🎉I love you! 💖";
+    document.getElementById("response").style.fontSize = "2em";
+    document.getElementById("response").style.color = "#ff9999";
+}
+
+function handleNo() {
+    document.body.style.backgroundColor = "#cccccc";
+    document.getElementById("response").innerHTML = "No? Are you sure? 😢 You're missing out! 😘";
+    document.getElementById("response").style.fontSize = "2em";
+    document.getElementById("response").style.color = "#999999";
+}
