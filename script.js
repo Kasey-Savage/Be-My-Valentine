1c710788415d14b5c34b83ff9c664d3fb213b7d1
Kasey-Savage <103228559+Kasey-Savage@users.noreply.github.com>
2025-01-27 16:45:37 -0800

    Fixed spacing issue

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function handleYes() {
     responseElem.style.display = "block";
     
     document.body.style.backgroundColor = "#ff3838";
-    document.getElementById("response").innerHTML = "Yay! I knew you'd say yes! 🎉I love you! 💖";
+    document.getElementById("response").innerHTML = "Yay! I knew you'd say yes! 🎉 I love you! 💖";
     document.getElementById("response").style.fontSize = "2em";
     document.getElementById("response").style.color = "#ff9999";
 }
@@ -11,7 +11,7 @@ function handleYes() {
 function handleNo() {
     const responseElem = document.getElementById("response");
     responseElem.style.display = "block";
-    
+
     document.body.style.backgroundColor = "#cccccc";
     document.getElementById("response").innerHTML = "No? Are you sure? 😢 You're missing out! 😘";
     document.getElementById("response").style.fontSize = "2em";
